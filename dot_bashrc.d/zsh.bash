ee77e14ac0f0c77f03aec9b534396dfaead46e15
Victor Bouvier-Deleau <victor@bouvier-deleau.fr>
2023-11-23 21:47:57 +0100

    [bash] Remove shebang

--- dot_bashrc.d/zsh.bash
+++ dot_bashrc.d/zsh.bash
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 if [[ $(hostname) == "toolbox" ]]; then
 	if [[ $(ps --no-header --pid=$PPID --format=comm) != "zsh" && -z ${BASH_EXECUTION_STRING} ]]; then
 		shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
