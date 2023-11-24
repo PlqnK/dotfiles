f4885a0ccadb6e826b202d3d47e10bb2991d2a0d
Victor Bouvier-Deleau <victor@bouvier-deleau.fr>
2023-11-24 16:36:11 +0100

    [bash] Change tabs to spaces

--- dot_bashrc.d/zsh.bash
+++ dot_bashrc.d/zsh.bash
@@ -1,6 +1,6 @@
 if [[ $(hostname) == "toolbox" ]]; then
-	if [[ $(ps --no-header --pid=$PPID --format=comm) != "zsh" && -z ${BASH_EXECUTION_STRING} ]]; then
-		shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
-		exec zsh $LOGIN_OPTION
-	fi
+  if [[ $(ps --no-header --pid=$PPID --format=comm) != "zsh" && -z ${BASH_EXECUTION_STRING} ]]; then
+    shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
+    exec zsh $LOGIN_OPTION
+  fi
 fi
