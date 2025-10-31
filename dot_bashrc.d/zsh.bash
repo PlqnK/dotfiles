adbbd1271a80d6ff286f8affa6970c86851bc8e6
Victor Bouvier-Deleau <victor@bouvier-deleau.fr>
2025-10-31 15:11:55 +0100

    Switch to WSL2 and Fedora instead of Ubuntu in WSL

--- dot_bashrc.d/zsh.bash
+++ dot_bashrc.d/zsh.bash
@@ -1,5 +1,6 @@
-if [[ $(hostname) == "toolbox" ]]; then
-  if [[ $(ps --no-header --pid=$PPID --format=comm) != "zsh" && -z ${BASH_EXECUTION_STRING} ]]; then
+# Switch to zsh if running in Toolbx or WSL but not when executing in Silverblue "system" shell
+if [[ -f /run/.toolboxenv || $(grep -i "WSL" /proc/sys/kernel/osrelease) ]]; then
+  if [[ $(ps --no-header --pid=$PPID --format=comm) != "zsh" && -z ${BASH_EXECUTION_STRING} && ${SHLVL} == 1 ]]; then
     shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
     exec zsh $LOGIN_OPTION
   fi
