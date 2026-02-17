45fbee5e96fae192eb2eb7ab2ed82afe768ab836
Victor Bouvier-Deleau <victor@bouvier-deleau.fr>
2026-02-18 00:41:31 +0100

    [zsh] Update to latest recommended shell drop-in from ArchWiki
    See https://wiki.archlinux.org/title/Fish#Modify_.bashrc_to_drop_into_fish

--- dot_bashrc.d/zsh.bash
+++ dot_bashrc.d/zsh.bash
@@ -1,6 +1,6 @@
 # Switch to zsh if running in Toolbx or WSL but not when executing in Silverblue "system" shell
 if [[ -f /run/.toolboxenv || $(grep -i "WSL" /proc/sys/kernel/osrelease) ]]; then
-  if [[ $(ps --no-header --pid=$PPID --format=comm) != "zsh" && -z ${BASH_EXECUTION_STRING} && ${SHLVL} == 1 ]]; then
+  if grep -qv 'zsh' /proc/$PPID/comm && [[ ${SHLVL} == [1,2] ]]; then
     shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
     exec zsh $LOGIN_OPTION
   fi
