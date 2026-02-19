e32eb9f017d9ca0cad85abe08ac286168ffdbcfe
Victor Bouvier-Deleau <victor@bouvier-deleau.fr>
2026-02-19 22:00:44 +0100

    [bash] Add interractive shell check to zsh drop in

--- dot_bashrc.d/zsh.bash
+++ dot_bashrc.d/zsh.bash
@@ -1,6 +1,6 @@
 # Switch to zsh if running in Toolbx or WSL but not when executing in Silverblue "system" shell
 if [[ -f /run/.toolboxenv || $(grep -i "WSL" /proc/sys/kernel/osrelease) ]]; then
-  if grep -qv 'zsh' /proc/$PPID/comm && [[ ${SHLVL} == [1,2] ]]; then
+  if grep -qv 'zsh' /proc/$PPID/comm && [[ ${SHLVL} == [1,2] ]] && [[ $- == *i* ]]; then
     shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
     exec zsh $LOGIN_OPTION
   fi
